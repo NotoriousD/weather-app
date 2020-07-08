7838343017ba3f4a1c9acd941264282da793a34d
Aabondd <aabondd@gmail.com>
2020-07-08 19:07:46 +0300

    Created pixabay_api fetch function

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,3 @@
 import pixabayApi from './pixabay_api_service';
-import weatherApi from './weather_api_service';
 
 pixabayApi.fetchPictures();
-weatherApi.fetchCurrentWeatherByCoord();
-weatherApi.fetchCurrentWeatherByCity();
-weatherApi.fetchWeatherForecastByCoord();
-weatherApi.fetchWeatherForecastByCity();
