91b432aca3e3ab9cd65e72c6614486956ab03a8c
KalinkinaD <dkalinina8@gmail.com>
2020-07-08 21:07:24 +0300

    input & slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { getGeolocation } from './components/geolocation';
 import './styles.css';
 import './css/page.scss';
-
+import slider from './components/d_sliderFavCity';
 getGeolocation();
