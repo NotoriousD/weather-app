c801930a7b2453b3f55932fd82be91901de93805
Michael <mykhaylo88@icloud.com>
2020-07-11 17:26:45 +0300

    added some funny stuf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import './css/fiveday.scss';
 import './components/5dayModule';
-// import './components/5dayHeading'
+import './components/5dayHeading'
 // import './components/5daySlider'
 
 import './components/chart';
