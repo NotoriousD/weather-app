0fad6dc9835ba6d3c92db427eebfb1756111898d
Michael <mykhaylo88@icloud.com>
2020-07-12 21:48:58 +0300

    added input logic

--- src/components/5dayModule.js
+++ src/components/5dayModule.js
@@ -1,58 +1,402 @@
-import './styles.css';
-import './css/fiveday.scss';
-import './media.css';
-import './css/page.scss';
-import './css/dariaK/inputAndSlider.scss';
-import './components/5dayModule';
-import './components/5dayHeading';
-import './components/chart';
-/* 
-import './components/searchAndSlider.js';
-import './components/api'; */
-import './components/pixabay_api_service';
-import './components/inputSearch';
-import pixabayApi from './components/pixabay_api_service';
-import './components/rendering_degree';
-import './components/display_quotes';
-import {
-  queryLayoutOneDay,
-  startOneDayLayout,
-} from './components/rendering_degree';
-import PNotify from '../node_modules/@pnotify/core/dist/PNotify';
-import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+import weatherApi from './weather_api_service';
 
-let store = {
-  state: 'One',
-};
+const moreInfo = document.querySelector('.pm_5day_more_info');
+const daysContainer = document.querySelector('.js-five-days');
 
-const input = document.querySelector('#d_search');
-const form = document.querySelector('#search-form');
+// --------при инпуте
+const searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-form.addEventListener('submit', submitForm);
+  const form = e.currentTarget;
+  const input = form.elements.query;
+  const inputValue = input.value;
 
-function submitForm(event) {
-  event.preventDefault();
-  if (input.value === '') {
-    PNotify.error({
-      title: 'NOTICE!',
-      text: 'Please write search city!',
-    });
-  }
-  if (store.state === '')
-    pixabayApi.fetchPictures().then(images => {
-      const number = getRandomInt(3);
-      document.body.style.backgroundImage = `url(${images.hits[number].largeImageURL})`;
-    });
-  queryLayoutOneDay(input.value);
-}
+  weatherApi.searchQuerry = inputValue;
+  daysContainer.innerHTML = '';
+  weatherApi.fetchWeatherForecastByCity().then(data => {
+    const list = data.list;
 
-startOneDayLayout();
+    for (let i = 0; i < list.length; i = i + 8) {
+      daysContainer.innerHTML += `
+                <li class="pm_info_item">
+                    <div class="pm_day_week">${getWeekDay(
+                      data.list[i].dt,
+                    )}</div>
+                    <div data-days="${getDayNumber(
+                      data.list[i].dt,
+                    )}" class="pm_day_month">${getMonthDay(
+        data.list[i].dt,
+      )}</div>
+                    <div class="pm_img_5days"><img src='https://openweathermap.org/img/w/${
+                      data.list[i].weather[0].icon
+                    }.png'></div>
+                    <div class="pm_min_max_label">
+                        <span>min</span>
+                        <span>max</span>
+                    </div>
+                    <div class="pm_min_max_item">
+                        <span>${Math.round(data.list[i].main.temp_min)}°</span>
+                        <span>${Math.round(data.list[i].main.temp_max)}°</span>
+                    </div>
+  
+                    <div id="pm_infoBtn" class="pm_more_info">more info</div>
+                </li>`;
+    }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+    const infoBtn = document.querySelectorAll('#pm_infoBtn');
+    const daysOfWeek = document.querySelectorAll('.pm_day_month');
+    const infoBlock = document.querySelector('.pm_more_info_block');
 
-pixabayApi.fetchPictures().then(images => {
-  const number = getRandomInt(3);
-  document.body.style.backgroundImage = `url(${images.hits[number].largeImageURL})`;
+    for (let i = 0; i < infoBtn.length; i++) {
+      infoBtn[i].addEventListener('click', event => {
+        moreInfo.innerHTML = '';
+
+        infoBlock.classList.add('pm_is_active');
+
+        list.forEach(item => {
+          let unix_timestamp = item.dt;
+          var date = new Date(unix_timestamp * 1000);
+          var hours = date.getHours();
+          var minutes = '0' + date.getMinutes();
+          var formattedTime = hours + ':' + minutes.substr(-2);
+          const dayz = date.getDate();
+
+          if (dayz != daysOfWeek[i].dataset.days) {
+            return;
+          } else {
+            moreInfo.insertAdjacentHTML(
+              'beforeend',
+              `<li class="pm_card_container">
+                                    <div class="pm_card_date">${formattedTime}</div>
+                                    <div class="pm_card_icon"><img src="https://openweathermap.org/img/w/${
+                                      item.weather[0].icon
+                                    }.png" alt=""></div>
+                                    <div class="pm_card_tmp">${Math.round(
+                                      item.main.temp,
+                                    )}°</div>
+                                    <div class="pm_card_grd_lvl"><img src="https://i.ibb.co/wJ0mfSh/wez1.png" alt="">${
+                                      item.main.grnd_level
+                                    } mm</div>
+                                    <div class="pm_card_hum"><img src="https://i.ibb.co/tDyKvnT/wez2.png" alt="">${
+                                      item.main.humidity
+                                    }%</div>
+                                    <div class="pm_card_wind"><img src="https://i.ibb.co/2nRWq6x/wez3.png" alt="">${
+                                      item.wind.speed
+                                    } m/s</div>
+                              </li>`,
+            );
+          }
+          scroll();
+        });
+        document
+          .querySelector('.pm_js_close_btn')
+          .addEventListener('click', () => {
+            infoBlock.classList.remove('pm_is_active');
+          });
+      });
+    }
+  });
 });
+// -------при гео
+
+navigator.geolocation.getCurrentPosition(
+  function (pos) {
+    daysContainer.innerHTML = '';
+    weatherApi
+      .fetchWeatherForecastByCoord(pos.coords.latitude, pos.coords.longitude)
+      .then(data => {
+        const list = data.list;
+
+        for (let i = 0; i < list.length; i = i + 8) {
+          daysContainer.innerHTML += `
+                <li class="pm_info_item">
+                    <div class="pm_day_week">${getWeekDay(
+                      data.list[i].dt,
+                    )}</div>
+                    <div data-days="${getDayNumber(
+                      data.list[i].dt,
+                    )}" class="pm_day_month">${getMonthDay(
+            data.list[i].dt,
+          )}</div>
+                    <div class="pm_img_5days"><img src='https://openweathermap.org/img/w/${
+                      data.list[i].weather[0].icon
+                    }.png'></div>
+                    <div class="pm_min_max_label">
+                        <span>min</span>
+                        <span>max</span>
+                    </div>
+                    <div class="pm_min_max_item">
+                        <span>${Math.round(data.list[i].main.temp_min)}°</span>
+                        <span>${Math.round(data.list[i].main.temp_max)}°</span>
+                    </div>
+  
+                    <div id="pm_infoBtn" class="pm_more_info">more info</div>
+                </li>`;
+        }
+
+        const infoBtn = document.querySelectorAll('#pm_infoBtn');
+        const daysOfWeek = document.querySelectorAll('.pm_day_month');
+        const infoBlock = document.querySelector('.pm_more_info_block');
+
+        for (let i = 0; i < infoBtn.length; i++) {
+          infoBtn[i].addEventListener('click', event => {
+            moreInfo.innerHTML = '';
+
+            infoBlock.classList.add('pm_is_active');
+
+            list.forEach(item => {
+              let unix_timestamp = item.dt;
+              var date = new Date(unix_timestamp * 1000);
+              var hours = date.getHours();
+              var minutes = '0' + date.getMinutes();
+              var formattedTime = hours + ':' + minutes.substr(-2);
+              const dayz = date.getDate();
+
+              if (dayz != daysOfWeek[i].dataset.days) {
+                return;
+              } else {
+                moreInfo.insertAdjacentHTML(
+                  'beforeend',
+                  `<li class="pm_card_container">
+                                    <div class="pm_card_date">${formattedTime}</div>
+                                    <div class="pm_card_icon"><img src="https://openweathermap.org/img/w/${
+                                      item.weather[0].icon
+                                    }.png" alt=""></div>
+                                    <div class="pm_card_tmp">${Math.round(
+                                      item.main.temp,
+                                    )}°</div>
+                                    <div class="pm_card_grd_lvl"><img src="https://i.ibb.co/wJ0mfSh/wez1.png" alt="">${
+                                      item.main.grnd_level
+                                    } mm</div>
+                                    <div class="pm_card_hum"><img src="https://i.ibb.co/tDyKvnT/wez2.png" alt="">${
+                                      item.main.humidity
+                                    }%</div>
+                                    <div class="pm_card_wind"><img src="https://i.ibb.co/2nRWq6x/wez3.png" alt="">${
+                                      item.wind.speed
+                                    } m/s</div>
+                              </li>`,
+                );
+              }
+              scroll();
+            });
+            document
+              .querySelector('.pm_js_close_btn')
+              .addEventListener('click', () => {
+                infoBlock.classList.remove('pm_is_active');
+              });
+          });
+        }
+      });
+  },
+  function (error) {
+    weatherApi.searchQuerry = 'Kiev';
+    daysContainer.innerHTML = '';
+    weatherApi.fetchWeatherForecastByCity().then(data => {
+      const list = data.list;
+
+      for (let i = 0; i < list.length; i = i + 8) {
+        daysContainer.innerHTML += `
+                  <li class="pm_info_item">
+                      <div class="pm_day_week">${getWeekDay(
+                        data.list[i].dt,
+                      )}</div>
+                      <div data-days="${getDayNumber(
+                        data.list[i].dt,
+                      )}" class="pm_day_month">${getMonthDay(
+          data.list[i].dt,
+        )}</div>
+                      <div class="pm_img_5days"><img src='https://openweathermap.org/img/w/${
+                        data.list[i].weather[0].icon
+                      }.png'></div>
+                      <div class="pm_min_max_label">
+                          <span>min</span>
+                          <span>max</span>
+                      </div>
+                      <div class="pm_min_max_item">
+                          <span>${Math.round(
+                            data.list[i].main.temp_min,
+                          )}°</span>
+                          <span>${Math.round(
+                            data.list[i].main.temp_max,
+                          )}°</span>
+                      </div>
+    
+                      <div id="pm_infoBtn" class="pm_more_info">more info</div>
+                  </li>`;
+      }
+
+      const infoBtn = document.querySelectorAll('#pm_infoBtn');
+      const daysOfWeek = document.querySelectorAll('.pm_day_month');
+      const infoBlock = document.querySelector('.pm_more_info_block');
+
+      for (let i = 0; i < infoBtn.length; i++) {
+        infoBtn[i].addEventListener('click', event => {
+          moreInfo.innerHTML = '';
+
+          infoBlock.classList.add('pm_is_active');
+
+          list.forEach(item => {
+            let unix_timestamp = item.dt;
+            var date = new Date(unix_timestamp * 1000);
+            var hours = date.getHours();
+            var minutes = '0' + date.getMinutes();
+            var formattedTime = hours + ':' + minutes.substr(-2);
+            const dayz = date.getDate();
+
+            if (dayz != daysOfWeek[i].dataset.days) {
+              return;
+            } else {
+              moreInfo.insertAdjacentHTML(
+                'beforeend',
+                `<li class="pm_card_container">
+                                      <div class="pm_card_date">${formattedTime}</div>
+                                      <div class="pm_card_icon"><img src="https://openweathermap.org/img/w/${
+                                        item.weather[0].icon
+                                      }.png" alt=""></div>
+                                      <div class="pm_card_tmp">${Math.round(
+                                        item.main.temp,
+                                      )}°</div>
+                                      <div class="pm_card_grd_lvl"><img src="https://i.ibb.co/wJ0mfSh/wez1.png" alt="">${
+                                        item.main.grnd_level
+                                      } mm</div>
+                                      <div class="pm_card_hum"><img src="https://i.ibb.co/tDyKvnT/wez2.png" alt="">${
+                                        item.main.humidity
+                                      }%</div>
+                                      <div class="pm_card_wind"><img src="https://i.ibb.co/2nRWq6x/wez3.png" alt="">${
+                                        item.wind.speed
+                                      } m/s</div>
+                                </li>`,
+              );
+            }
+            scroll();
+          });
+          document
+            .querySelector('.pm_js_close_btn')
+            .addEventListener('click', () => {
+              infoBlock.classList.remove('pm_is_active');
+            });
+        });
+      }
+    });
+  },
+);
+
+function getWeekDay(number) {
+  const a = new Date(number * 1000);
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dayOfWeek = days[a.getDay()];
+  return dayOfWeek;
+}
+
+function getMonthDay(number) {
+  const a = new Date(number * 1000);
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const month = months[a.getMonth()];
+  const day = a.getDate();
+  const validFormat = `${day} ${month}`;
+  return validFormat;
+}
+
+function getDayNumber(number) {
+  const a = new Date(number * 1000);
+  const day = a.getDate();
+  return day;
+}
+
+const prevArrow = document.querySelector('#pm_js_prev');
+const nextArrow = document.querySelector('#pm_js_next');
+let currentStep = 0;
+
+nextArrow.addEventListener('click', handlerNextHour);
+prevArrow.addEventListener('click', handlerPrevHour);
+
+function handlerNextHour(event) {
+  if (currentStep >= 4) {
+    return;
+  }
+  currentStep++;
+  setPositionRight();
+}
+
+function handlerPrevHour(event) {
+  if (currentStep <= 0) {
+    return;
+  }
+  currentStep--;
+  setPositionLeft();
+}
+
+function setPositionRight() {
+  moreInfo.style.transform += `translateX(-140px)`;
+  moreInfo.style.transitionDuration = '500ms';
+}
+
+function setPositionLeft() {
+  moreInfo.style.transform += `translateX(140px)`;
+  moreInfo.style.transitionDuration = '500ms';
+}
+// -------------------------------------------
+// Для мобильной версии
+
+const fivedays = document.querySelector('.pm_days');
+const prevArrowm = document.querySelector('.pm_js_prev_mobile');
+const nextArrowm = document.querySelector('.pm_js_next_mobile');
+let currentStepm = 0;
+/* 
+nextArrowm.addEventListener('click', handlerNextHourm);
+prevArrowm.addEventListener('click', handlerPrevHourm); */
+
+function handlerNextHourm(event) {
+  if (currentStep >= 3) {
+    return;
+  }
+  currentStep++;
+  setPositionRightm();
+}
+
+function handlerPrevHourm(event) {
+  if (currentStep <= 0) {
+    return;
+  }
+  currentStep--;
+  setPositionLeftm();
+}
+
+function setPositionRightm() {
+  fivedays.style.transform += `translateX(-70px)`;
+  fivedays.style.transitionDuration = '500ms';
+}
+
+function setPositionLeftm() {
+  fivedays.style.transform += `translateX(70px)`;
+  fivedays.style.transitionDuration = '500ms';
+}
+
+function scroll() {
+  window.scrollTo({
+    top: window.innerHeight + window.scrollY,
+    behavior: 'smooth',
+  });
+}
