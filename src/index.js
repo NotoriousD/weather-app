0ca5dcacfdb61af4d447aff8cf113086bfa937e1
KalinkinaD <dkalinina8@gmail.com>
2020-07-12 18:07:22 +0300

    Slider v.2.0 and function for localStorage

--- src/index.js
+++ src/index.js
@@ -4,22 +4,25 @@ import './media.css';
 import './css/page.scss';
 import './css/dariaK/inputAndSlider.scss';
 import './components/5dayModule';
-import './components/5dayHeading'
-import './components/chart';/* 
+import './components/5dayHeading';
+import './components/chart';
 import './components/searchAndSlider.js';
-import './components/api'; */
+// import './components/api'; */
 import './components/pixabay_api_service';
 import './components/inputSearch';
 import pixabayApi from './components/pixabay_api_service';
 import './components/rendering_degree';
 import './components/display_quotes';
-import {queryLayoutOneDay, startOneDayLayout} from './components/rendering_degree'
-import PNotify from '../node_modules/@pnotify/core/dist/PNotify'
-import '../node_modules/@pnotify/core/dist/BrightTheme.css'
+import {
+  queryLayoutOneDay,
+  startOneDayLayout,
+} from './components/rendering_degree';
+import PNotify from '../node_modules/@pnotify/core/dist/PNotify';
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
 
 let store = {
-  state: 'One'
-}
+  state: 'One',
+};
 
 const input = document.querySelector('#d_search');
 const form = document.querySelector('#search-form');
@@ -34,12 +37,12 @@ function submitForm(event) {
       text: 'Please write search city!',
     });
   }
-  if(store.state === '')
-  pixabayApi.fetchPictures().then(images => {
-    const number = getRandomInt(3);
-    document.body.style.backgroundImage = `url(${images.hits[number].largeImageURL})`;
-  });
-  queryLayoutOneDay(input.value)
+  if (store.state === '')
+    pixabayApi.fetchPictures().then(images => {
+      const number = getRandomInt(3);
+      document.body.style.backgroundImage = `url(${images.hits[number].largeImageURL})`;
+    });
+  queryLayoutOneDay(input.value);
 }
 
 startOneDayLayout();
@@ -52,5 +55,3 @@ pixabayApi.fetchPictures().then(images => {
   const number = getRandomInt(3);
   document.body.style.backgroundImage = `url(${images.hits[number].largeImageURL})`;
 });
-
-
